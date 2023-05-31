ef116a124b7a95bad4ddbdcc9b08280c77753729
canonen <106859600+canonen@users.noreply.github.com>
2023-05-31 10:01:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1059,7 +1059,8 @@ def checkAlarms(user_id):
     now = now.strftime("%Y-%m-%d %H:%M")
 
     last_user_alarm_date = cursor.execute(f"select date from Alarms where user_id = '{user_id}'and date < '{now}' order by date desc").fetchone()
-
+    if last_user_alarm_date == None:
+        return "ilk alarmınızın saati henüz gelmemiş veya hiç alarm kurmamışsınız..."
     type_list = []
     user_alarm_types = cursor.execute(f"SELECT type FROM Alarms WHERE user_id = ? and date = ?",
                                       (user_id, last_user_alarm_date[0])).fetchall()
